adbce944ab2d5f61530aefcabfb3064fb46bae7c
hzrd149 <github@hzrd149.com>
2024-02-03 19:03:48 +0000

    Update to v0.38.0

--- scripts/procedures/migrations.ts
+++ scripts/procedures/migrations.ts
@@ -1,4 +1,4 @@
 import { compat, types as T } from "../deps.ts";
 
 export const migration: T.ExpectedExports.migration = compat.migrations
-    .fromMapping({}, "0.37.1.0" );
+    .fromMapping({}, "0.38.0.0" );
