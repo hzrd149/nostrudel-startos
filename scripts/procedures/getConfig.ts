ebe6ee9a475cab9c6185f650d9ea388b24a0706e
Mariusz Kogen <k0gen@pm.me>
2024-02-12 18:02:50 +0100

    no cache dependency

--- scripts/procedures/getConfig.ts
+++ scripts/procedures/getConfig.ts
@@ -2,21 +2,4 @@
 
 import { compat, types as T } from "../deps.ts";
 
-export const getConfig: T.ExpectedExports.getConfig = compat.getConfig({
-  "enable-cache-relay": {
-    name: "Use local relay for caching",
-    description:
-      "If you enable the option, noStrudel will cache Nostr events on your personal Nostr relay, which is fast and makes the cache available to all your logged in clients. If you disable this option, noStrudel will instead cache Nostr events in the browser's storage, which is slower and does not sync across clients.",
-    type: "boolean",
-    default: false,
-  },
-  "cache-relay": {
-    name: "Cache Relay",
-    description: "A local relay to use for caching events",
-    type: "pointer",
-    subtype: "package",
-    "package-id": "nostr",
-    target: "lan-address",
-    interface: "websocket",
-  },
-});
+export const getConfig: T.ExpectedExports.getConfig = compat.getConfig({});
